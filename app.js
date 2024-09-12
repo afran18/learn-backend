1fadbc72b859eb8cb828991b59b1580532144d87
afran18 <afran.vk18@gmail.com>
2024-09-12 21:47:34 +0530

    Sequelize more enhancements

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
 const sequelize = require('./util/database');
+const Product = require('./models/product');
+const User = require('./models/user');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
 
 const app = express();
 
@@ -13,24 +17,54 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const Sequelize = require('sequelize');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use((req,res, next) => {
+    User.findByPk(1)
+    .then(user => {
+        req.user = user;
+        next();
+    })
+    .catch(err => {
+        console.log(err);
+        
+    })
+})
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-sequelize.sync().then( result => {
-    //console.log(result);
-}).catch(err => {
-    console.log(err);
-    
-})
+Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE'});
+User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, {through: CartItem});
+Product.belongsToMany(Cart, {through: CartItem});
 
+sequelize
+    .sync({force: true})
+    //.sync()
+    .then( result => {
+        return User.findByPk(1);
+        //console.log(result);
+    })
+    .then(user => {
+        if(!user) {
+            User.create({name: 'Afran', email: 'afran@email.com'});
+        }
+        return user;
+    })
+    .then( user => {
+        console.log(user);
+        app.listen(3000);
+        console.log("Server started");
+    })
+    .catch(err => {
+        console.log(err); 
+});
 
-app.listen(3000);
-
-console.log("Server started");
 
