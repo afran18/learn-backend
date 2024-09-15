6766b0d3c7ceec58ba59b254718f865762e395df
afran18 <afran.vk18@gmail.com>
2024-09-15 23:00:24 +0530

    Sequelize Done

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const Product = require('./models/product');
 const User = require('./models/user');
 const Cart = require('./models/cart');
 const CartItem = require('./models/cart-item');
+const Order = require('./models/order');
+const OrderItem = require('./models/order-item');
 
 const app = express();
 
@@ -17,54 +19,54 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-const Sequelize = require('sequelize');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use((req,res, next) => {
-    User.findByPk(1)
+
+app.use((req, res, next) => {
+  User.findByPk(1)
     .then(user => {
-        req.user = user;
-        next();
+      req.user = user;
+      next();
     })
-    .catch(err => {
-        console.log(err);
-        
-    })
-})
+    .catch(err => console.log(err));
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE'});
+Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
 User.hasMany(Product);
 User.hasOne(Cart);
 Cart.belongsTo(User);
-Cart.belongsToMany(Product, {through: CartItem});
-Product.belongsToMany(Cart, {through: CartItem});
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
+Order.belongsTo(User);
+User.hasMany(Order);
+Order.belongsToMany(Product, { through: OrderItem });
 
 sequelize
-    .sync({force: true})
-    //.sync()
-    .then( result => {
-        return User.findByPk(1);
-        //console.log(result);
-    })
-    .then(user => {
-        if(!user) {
-            User.create({name: 'Afran', email: 'afran@email.com'});
-        }
-        return user;
-    })
-    .then( user => {
-        console.log(user);
-        app.listen(3000);
-        console.log("Server started");
-    })
-    .catch(err => {
-        console.log(err); 
-});
-
-
+  // .sync({ force: true })
+  .sync()
+  .then(result => {
+    return User.findByPk(1);
+    // console.log(result);
+  })
+  .then(user => {
+    if (!user) {
+      return User.create({ name: 'Max', email: 'test@test.com' });
+    }
+    return user;
+  })
+  .then(user => {
+    // console.log(user);
+    return user.createCart();
+  })
+  .then(cart => {
+    app.listen(3000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
